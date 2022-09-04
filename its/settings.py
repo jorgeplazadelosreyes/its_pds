5896d4b8310484d35e94691f52e907a3d794b79f
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-09-04 18:06:58 -0300

    line jump and requirements updates

--- its/settings.py
+++ its/settings.py
@@ -128,6 +128,6 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, "static")
 )
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"import django_on_heroku
+import django_on_heroku
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 django_on_heroku.settings(locals())
