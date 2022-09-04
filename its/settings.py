c0580f91e8981306b89e965a269b93db03bdddf9
mjanas1 <mjanas@miuandes.cl>
2022-09-04 20:23:23 -0300

    Procfile release

--- its/settings.py
+++ its/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,5 +131,4 @@ STATICFILES_DIR = (
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-import django_heroku
 django_heroku.settings(locals())
