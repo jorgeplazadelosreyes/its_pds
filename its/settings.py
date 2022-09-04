e05fed29c85659606d07dd7c7b58680197b7f4d0
mjanas1 <mjanas@miuandes.cl>
2022-09-04 20:54:40 -0300

    Try with gunicorn

--- its/settings.py
+++ its/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,4 +130,4 @@ STATICFILES_DIR = (
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-django_heroku.settings(locals())
+
