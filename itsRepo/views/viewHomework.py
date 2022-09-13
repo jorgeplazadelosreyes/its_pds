b8a71bf59aa96ce1812df4b1b3730a12eae903f0
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-09-13 11:12:18 -0300

    Filled previous statement

--- itsRepo/views/viewHomework.py
+++ itsRepo/views/viewHomework.py
@@ -8,7 +8,9 @@ from django.shortcuts import render, redirect
 class ViewHomework(View):
     def post(self, request, homework_id):
         dcl = request.POST.get("diagram")
+        s_statement = request.POST.get("str_statement")
         homework = Homework.objects.get(id=homework_id)
         homework.diagram = str(dcl)
+        homework.statement_text = str(s_statement)
         homework.save()
         return HttpResponse("ok", status = 200)
