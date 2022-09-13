1d8fb2e5f92f09d287e4c8c989138567cc56704c
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-09-13 00:25:19 -0300

    statement_text and model changes

--- itsRepo/models/homework.py
+++ itsRepo/models/homework.py
@@ -4,4 +4,6 @@ from django.db import models
 class Homework(models.Model):
     diagram = models.JSONField()
     title = models.CharField(max_length=20)
+    statement_text = models.TextField(null=True, blank=True, default="")
+    statement_file = models.FileField(null=True)
     difficulty = models.IntegerField(null=True)
